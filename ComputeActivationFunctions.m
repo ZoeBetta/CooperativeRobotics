17a4bed4ed076a713c3bad339199b0f3f4e1b1f0
ZoeBetta <77151364+ZoeBetta@users.noreply.github.com>
2021-12-21 09:50:50 +0000

    Add files via upload

---
+++ ComputeActivationFunctions.m
@@ -0,0 +1,8 @@
+function [uvms] = ComputeActivationFunctions(uvms, mission)
+% compute the activation functions here
+
+% arm tool position control
+% always active
+uvms.A.t = eye(6);
+uvms.A.v_l= eye(3);
+uvms.A.v_a=eye(3);
---
+++ ComputeTaskReferences.m
@@ -0,0 +1,17 @@
+function [uvms] = ComputeTaskReferences(uvms, mission)
+% compute the task references here
+
+% reference for tool-frame position control task
+[ang, lin] = CartError(uvms.vTg , uvms.vTt);
+uvms.xdot.t = 0.2 * [ang; lin];
+% limit the requested velocities...
+uvms.xdot.t(1:3) = Saturate(uvms.xdot.t(1:3), 0.2);
+uvms.xdot.t(4:6) = Saturate(uvms.xdot.t(4:6), 0.2);
+
+% reference for vehicle position control task - projected on the world
+% frame
+[ang_v, lin_v] = CartError(uvms.wTggv , uvms.wTv);
+uvms.xdot.v_l=0.2*lin_v;
+uvms.xdot.v_a=0.2*ang_v;
+uvms.xdot.v_l = Saturate(uvms.xdot.v_l, 0.2);
+uvms.xdot.v_a = Saturate(uvms.xdot.v_a, 0.2);
