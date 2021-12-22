7ef471d846748fe91a3ac177907ea764ad6f8f34
ZoeBetta <77151364+ZoeBetta@users.noreply.github.com>
2021-12-22 12:40:18 +0000

    Add files via upload

---
+++ ComputeActivationFunctions.m
@@ -0,0 +1,35 @@
+function [uvms] = ComputeActivationFunctions(uvms, mission)
+% compute the activation functions here
+    switch mission.phase
+        case 1  
+            uvms.Ap.ha = 1;
+            uvms.Ap.ma = 1;
+            uvms.Ap.a = 0;
+        case 2
+            uvms.Ap.ha = 1;
+            uvms.Ap.ma = 0;
+            uvms.Ap.a = 1; %IncreasingBellShapedFunction(0, 1, 0, 1, mission.phase_time);
+            uvms.Ap.o=1;
+    end
+% arm tool position control
+% always active
+uvms.A.t = eye(6);
+
+uvms.A.v_l= eye(3);
+uvms.A.v_a=eye(3);
+
+
+%horizontal attitude
+uvms.A.ha = IncreasingBellShapedFunction(0.2, 0.4, 0, 1, norm(uvms.v_rho_ha))*uvms.Ap.ha;
+
+
+%minimum altitude
+uvms.Ap.ma = 1;
+uvms.A.ma = DecreasingBellShapedFunction(0.5, 1, 0, 1, uvms.a)*uvms.Ap.ma; %1 è la distanza massima
+
+
+%altitude
+uvms.A.a = 1*uvms.Ap.a;
+
+%orientation equality, I want the two directions to be exactly the same
+uvms.A.o=1*uvms.Ap.o;
